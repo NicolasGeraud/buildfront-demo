5d3bb613d01f7609530f5601e5befd0f4a954036
Nicolas Géraud <nicolas.geraud@gmail.com>
2014-11-23 18:27:18 +0100

    ajout du sass et de la minification des css

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,11 @@ module.exports = function(grunt) {
         // Metadata.
         pkg: grunt.file.readJSON('package.json'),
 
+        properties: {
+            app: '.',
+            dist: './dist'
+        },
+
         // Task configuration.
         wiredep: {
             task: {
@@ -18,36 +23,160 @@ module.exports = function(grunt) {
                 options: {
                     port: 9000,
                     hostname: 'localhost',
-                    livereload: 35729,
+                    livereload: 35728,
+                    base: [
+                        '.tmp',
+                        '<%= properties.app %>'
+                    ],
                     open: true
                 }
             }
         },
 
+        // Compiles Sass to CSS and generates necessary files if requested
+        compass: {
+            options: {
+                sassDir: '<%= properties.app %>/styles',
+                cssDir: '.tmp/styles',
+                generatedImagesDir: '.tmp/images/generated',
+                imagesDir: '<%= properties.app %>/images',
+                javascriptsDir: '<%= properties.app %>/scripts',
+                fontsDir: '<%= properties.app %>/styles/fonts',
+                importPath: [
+                    '<%= properties.app %>/bower_components/bootstrap-sass-official/assets/stylesheets',
+                    '<%= properties.app %>/bower_components/bootstrap-sass-official/assets/fonts/bootstrap'
+                    ],
+                httpImagesPath: '/images',
+                httpGeneratedImagesPath: '/images/generated',
+                httpFontsPath: '/styles/fonts',
+                relativeAssets: false,
+                assetCacheBuster: false,
+                raw: 'Sass::Script::Number.precision = 10\n'
+            },
+            dist: {
+                options: {
+                    generatedImagesDir: '<%= properties.dist %>/images/generated'
+                }
+            },
+            server: {
+                options: {
+                    debugInfo: false
+                }
+            }
+        },
+
+        // Reads HTML for usemin blocks to enable smart builds that automatically
+        // concat, minify and revision files. Creates configurations in memory so
+        // additional tasks can operate on them
+        useminPrepare: {
+            html: '<%= properties.app %>/index.html',
+            options: {
+                dest: '<%= properties.dist %>'
+            }
+        },
+        // Performs rewrites based on rev and the useminPrepare configuration
+        usemin: {
+            html: ['<%= properties.dist %>/{,*/}*.html'],
+            css: ['<%= properties.dist %>/styles/{,*/}*.css'],
+             options: {
+                assetsDirs: ['<%= properties.dist %>']
+            }
+        },
+
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [
+                        '.tmp',
+                        '<%= properties.dist %>/*',
+                        '!<%= properties.dist %>/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+
+        // Copies remaining files to places other tasks can use
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= properties.app %>',
+                    dest: '<%= properties.dist %>',
+                    src: [
+                        '*.html',
+                        'components/**/*',
+                        'bower_components/**/*',
+                        'images/**/*'
+                    ]
+                }]
+            }
+        },
+
+        // Add vendor prefixed styles
+        autoprefixer: {
+            options: {
+                browsers: ['last 1 version']
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: '.tmp/styles/',
+                    src: '{,*/}*.css',
+                    dest: '.tmp/styles/'
+                }]
+            }
+        },
+
+
         watch: {
+            compass: {
+                files: ['<%= properties.app %>/styles/{,*/}*.{scss,sass}'],
+                tasks: ['compass:server', 'autoprefixer']
+            },
+
+            gruntfile: {
+                files: ['Gruntfile.js']
+            },
+
+
             livereload: {
                 options: {
                     livereload: '<%= connect.server.options.livereload %>'
                 },
                 files: [
-                    'index.html',
-                    'styles/app.css'
+                    '<%= properties.app %>/{,*/}*.html',
+                    '.tmp/styles/{,*/}*.css'
                 ]
 
             }
         }
     });
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks("grunt-wiredep");
-    grunt.loadNpmTasks("grunt-contrib-connect");
-    grunt.loadNpmTasks("grunt-contrib-watch");
+    require('load-grunt-tasks')(grunt);
+
+
+    grunt.registerTask('build', [
+        'clean:dist',
+        'clean:server',
+        'wiredep',
+        'useminPrepare',
+        'compass:server',
+        'autoprefixer',
+        'concat',
+        'copy:dist',
+        'cssmin',
+        'usemin'
+    ]);
 
     // Default task.
     grunt.registerTask('default', [
-        "wiredep",
-        "connect:server",
-        "watch"
+        'build',
+        'connect:server',
+        'watch'
     ]);
 
 };
